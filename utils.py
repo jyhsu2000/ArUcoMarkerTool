904b01731186e32aca55f0c6e61d69394c239871
jyhsu <jyhsu2000@gmail.com>
2022-02-12 16:23:52 +0800

    Create embed_img and create_text_pad

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import time
 from collections import deque
 
 import cv2
+import numpy as np
 
 
 class Singleton(type):
@@ -96,3 +97,40 @@ class CameraLooper(threading.Thread):
 
     def read(self):
         return self.ret, self.frame
+
+
+def embed_img(src_img: np.array, dest_img: np.array, dest_points: list, alpha: float = 1) -> np.array:
+    h, w, _ = dest_img.shape
+    # 座標
+    src_points = np.array([(0, 0), (0, h), (w, h), (w, 0)])
+    dest_points = np.array(dest_points)
+    # 計算轉換矩陣
+    homo, status = cv2.findHomography(src_points, dest_points)
+    # 轉換圖片座標
+    src_img = cv2.resize(src_img, (w, h))
+    p_src_img = cv2.warpPerspective(src_img, homo, (w, h))
+    # 建立遮罩
+    white_pad = np.full((h, w), 255, np.uint8)
+    fg_mask = cv2.warpPerspective(white_pad, homo, (w, h))
+    bg_mask = cv2.bitwise_not(fg_mask)
+    # 合成
+    masked_fg_img = cv2.bitwise_or(p_src_img, p_src_img, mask=fg_mask)
+    masked_bg_img = cv2.bitwise_or(dest_img, dest_img, mask=bg_mask)
+    masked_img = cv2.bitwise_or(masked_fg_img, masked_bg_img)
+    output = cv2.addWeighted(masked_img, alpha, dest_img, 1 - alpha, 0)
+
+    return output
+
+
+def create_text_pad(text: str = 'Text', text_color=(0, 255, 255), bg_color=(255, 0, 0)) -> np.array:
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text_scale = 5
+    thickness = 5
+
+    (w, h), _ = cv2.getTextSize(text, font, text_scale, thickness)
+
+    text_pad = np.zeros((h + 30, w, 3), np.uint8)
+    text_pad[:] = bg_color
+    text_pad = cv2.putText(text_pad, text, (5, h + 15), font, text_scale, text_color, thickness)
+
+    return text_pad
