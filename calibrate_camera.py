96df32fe20d40337b0093100a9070ced3b51799d
jyhsu <jyhsu2000@gmail.com>
2022-03-12 16:32:51 +0800

    Make chessboard configable

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import copy
 import os.path
+import re
 import threading
 import time
 from collections import deque
+from dataclasses import dataclass
 
 import PySimpleGUI as sg
 import cv2
@@ -20,14 +22,27 @@ thumbnail_size = (400, 300)
 # 找棋盤格角點
 # 設置尋找亞像素角點的參數，採用的停止準則是最大循環次數30和最大誤差容限0.001
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)  # 阈值
-# 棋盤格模板規格
-w = 9  # 10 - 1
-h = 6  # 7  - 1
-# 世界坐標系中的棋盤格點,例如(0,0,0), (1,0,0), (2,0,0) ....,(8,5,0)，去掉Z坐標，記為二維矩陣
-objp = np.zeros((w * h, 3), np.float32)
-objp[:, :2] = np.mgrid[0:w, 0:h].T.reshape(-1, 2)
-square_size_mm = 18.1
-objp = objp * square_size_mm
+
+
+@dataclass
+class Chessboard:
+    # 棋盤格模板規格
+    w: int
+    h: int
+    square_size_mm: float
+
+    @property
+    def objp(self):
+        # 世界坐標系中的棋盤格點,例如(0,0,0), (1,0,0), (2,0,0) ....,(8,5,0)，去掉Z坐標，記為二維矩陣
+        objp = np.zeros((self.w * self.h, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:self.w, 0:self.h].T.reshape(-1, 2)
+        square_size_mm = 24.6  # 棋盤格的寬度（務必正確設定，避免影響距離估算）
+        objp = objp * square_size_mm
+        return objp
+
+
+# 預設棋盤格模板規格
+default_chessboard = Chessboard(w=9, h=6, square_size_mm=24.6)
 
 
 def update_calibration_image_df(window, calibration_image_df: pd.DataFrame) -> pd.DataFrame:
@@ -53,7 +68,7 @@ def update_calibration_image_df(window, calibration_image_df: pd.DataFrame) -> p
     return calibration_image_df
 
 
-def detect_chessboard(window, filename, image=None):
+def detect_chessboard(window, chessboard: Chessboard, filename, image=None):
     if image is None:
         file_path = os.path.join(calibration_images_path, filename)
         image = cv2.imread(file_path)
@@ -62,7 +77,7 @@ def detect_chessboard(window, filename, image=None):
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # 找到棋盤格角點
-    ret, corners = cv2.findChessboardCorners(gray, (w, h), None)
+    ret, corners = cv2.findChessboardCorners(gray, (chessboard.w, chessboard.h), None)
     window.write_event_value('update_chessboard_detect_result', (filename, ret))
     if ret:
         # 在原角點的基礎上尋找亞像素角點
@@ -71,23 +86,23 @@ def detect_chessboard(window, filename, image=None):
         # objpoints.append(objp)
         # imgpoints.append(corners)
         # 將角點在圖像上顯示
-        cv2.drawChessboardCorners(image, (w, h), corners, ret)
+        cv2.drawChessboardCorners(image, (chessboard.w, chessboard.h), corners, ret)
 
     return ret, corners, image, gray,
 
 
-def update_thumbnail_images(window, filename: str):
+def update_thumbnail_images(window, chessboard: Chessboard, filename: str):
     file_path = os.path.join(calibration_images_path, filename)
     image = cv2.imread(file_path)
     thumbnail_image = imutils.resize(image, width=thumbnail_size[0], height=thumbnail_size[1])
     window.write_event_value('update_thumbnail_image', thumbnail_image)
 
-    ret, corners, image_with_marker, gray = detect_chessboard(window, filename, image)
+    ret, corners, image_with_marker, gray = detect_chessboard(window, chessboard, filename, image)
     thumbnail_image_with_marker = imutils.resize(image_with_marker, width=thumbnail_size[0], height=thumbnail_size[1])
     window.write_event_value('update_thumbnail_image_with_marker', thumbnail_image_with_marker)
 
 
-def calibrate(window, calibration_image_df: pd.DataFrame):
+def calibrate(window, chessboard: Chessboard, calibration_image_df: pd.DataFrame):
     row_count = len(calibration_image_df)
 
     # 儲存棋盤格角點的世界坐標和圖像坐標對
@@ -96,10 +111,10 @@ def calibrate(window, calibration_image_df: pd.DataFrame):
 
     for idx, row in calibration_image_df.iterrows():
         window.write_event_value('update_progress', (idx + 1, row_count))
-        ret, corners, image_with_marker, gray = detect_chessboard(window, row['filename'])
+        ret, corners, image_with_marker, gray = detect_chessboard(window, chessboard, row['filename'])
         if ret:
             # 追加進入世界三維點和平面二維點中
-            obj_points.append(objp)
+            obj_points.append(chessboard.objp)
             img_points.append(corners)
 
     if len(img_points) == 0:
@@ -119,6 +134,8 @@ def calibrate(window, calibration_image_df: pd.DataFrame):
 
 
 def main():
+    chessboard = copy.deepcopy(default_chessboard)
+
     calibration_image_df = pd.DataFrame({
         'filename': [],
         'chessboard': '',
@@ -143,6 +160,15 @@ def main():
                 [sg.Image(filename='', key='thumbnail', size=(400, 1))],
                 [sg.Image(filename='', key='thumbnail_with_marker', size=(400, 1))],
                 [sg.Button('Delete selected image', key='delete_selected_image', enable_events=True, button_color=('white', 'red'), font='Helvetica 14', expand_x=True, disabled=True)],
+                [
+                    sg.Text('Chessboard size (w*h):', size=(20, 1), font='Helvetica 14', justification='right'),
+                    sg.InputText(key='w_input', size=(4, 1), font='Helvetica 14', justification='center', enable_events=True, default_text=default_chessboard.w),
+                    sg.Text('x', font='Helvetica 14', justification='center'),
+                    sg.InputText(key='h_input', size=(4, 1), font='Helvetica 14', justification='center', enable_events=True, default_text=default_chessboard.h),
+                ], [
+                    sg.Text('Square size (mm):', size=(20, 1), font='Helvetica 14', justification='right'),
+                    sg.InputText(key='square_size_input', size=(10, 1), font='Helvetica 14', justification='center', enable_events=True, default_text=default_chessboard.square_size_mm),
+                ],
                 [
                     sg.Button('Calibrate', key='calibrate', font='Helvetica 20', enable_events=True),
                     sg.ProgressBar(max_value=10, orientation='h', size=(20, 20), key='progress'),
@@ -182,7 +208,7 @@ def main():
                     selected_row_index = None
                 if selected_row_index is not None:
                     selected_filename = calibration_image_df.loc[selected_row_index, 'filename']
-                    thread = threading.Thread(target=update_thumbnail_images, args=(window, selected_filename), daemon=True)
+                    thread = threading.Thread(target=update_thumbnail_images, args=(window, chessboard, selected_filename), daemon=True)
                     thread.start()
                     window['delete_selected_image'].update(disabled=False)
                 else:
@@ -218,15 +244,57 @@ def main():
                 calibration_image_df = update_calibration_image_df(window, calibration_image_df)
                 window.write_event_value('table', [None])
 
+            if event == 'w_input':
+                w_input = values['w_input']
+                if len(w_input) > 2:
+                    w_input = w_input[:2]
+                else:
+                    if re.match(r'^\d*$', w_input):
+                        if re.match(r'^\d+$', w_input):
+                            chessboard.w = int(w_input)
+                    else:
+                        w_input = chessboard.w
+                window['w_input'].update(w_input)
+
+            if event == 'h_input':
+                h_input = values['h_input']
+                if len(h_input) > 2:
+                    h_input = h_input[:2]
+                else:
+                    if re.match(r'^\d*$', h_input):
+                        if re.match(r'^\d+$', h_input):
+                            chessboard.h = int(h_input)
+                    else:
+                        h_input = chessboard.h
+                window['h_input'].update(h_input)
+
+            if event == 'square_size_input':
+                square_size_input = values['square_size_input']
+                if len(square_size_input) > 7:
+                    square_size_input = square_size_input[:7]
+                else:
+                    if re.match(r'^\d*\.?\d*$', square_size_input):
+                        if re.match(r'^\d+\.?\d*$', square_size_input):
+                            chessboard.square_size_mm = float(square_size_input)
+                    else:
+                        square_size_input = chessboard.square_size_mm
+                window['square_size_input'].update(square_size_input)
+
             if event == 'calibrate':
+                window['w_input'].update(chessboard.w, disabled=True)
+                window['h_input'].update(chessboard.h, disabled=True)
+                window['square_size_input'].update(chessboard.square_size_mm, disabled=True)
                 window['calibrate'].update(disabled=True)
-                thread = threading.Thread(target=calibrate, args=(window, calibration_image_df), daemon=True)
+                thread = threading.Thread(target=calibrate, args=(window, chessboard, calibration_image_df), daemon=True)
                 thread.start()
 
             if event == 'calibrate_finished':
                 window['progress'].update_bar(1, max=1)
                 custom_message = values['calibrate_finished']
                 sg.popup(custom_message)
+                window['w_input'].update(disabled=False)
+                window['h_input'].update(disabled=False)
+                window['square_size_input'].update(disabled=False)
                 window['calibrate'].update(disabled=False)
 
             if event == 'update_progress':
